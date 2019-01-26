0230531ab5653ba477f4f934eb362d6ac1ca75f7
firstonline <fffirstonline@gmail.com>
2019-01-26 15:48:45 +0000

    fixed skill tree

--- TheCoders/Assets/Scripts/Upgrade/SkillTree.cs
+++ TheCoders/Assets/Scripts/Upgrade/SkillTree.cs
@@ -25,7 +25,7 @@ public class SkillTree : MonoBehaviour
 
 		for (int i = 0; i < m_techUpgradesRanking.Count; i++)
 		{
-			m_techUpgradesRanking.Add(m_techUpgradesRanking[i], 0);
+			m_techUpgradesRanking.Add(m_techUpgrades[i], 0);
 		}
 
 		for (int i = 0; i < m_rocketUpgrades.Count; i++)
