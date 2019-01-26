0d5d0a97a5ef91bb70d935e78f001716b8b08663
firstonline <fffirstonline@gmail.com>
2019-01-26 14:40:08 +0000

    initial skill tree

--- TheCoders/Assets/Scripts/Upgrade/IUpgrade.cs
+++ TheCoders/Assets/Scripts/Upgrade/IUpgrade.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public abstract class IUpgrade : ScriptableObject
 {
+	public Image UpgradeImage;
 	public abstract UpgradeType UpgradeType { get; }
 	public abstract void ApplyUpgrade();
 	public abstract void RemoveUpgrade();
---
+++ TheCoders/Assets/Scripts/Upgrade/SkillTree.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SkillTree : MonoBehaviour
+{
+	[SerializeField] private List<PopulationUpgrade> m_populationUpgrades;
+	[SerializeField] private List<TechUpgrade> m_techUpgrades;
+	[SerializeField] private List<RocketUpgrade> m_rocketUpgrades;
+
+	
+}
