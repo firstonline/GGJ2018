e36120ab061426627f0b430daff6a1508ec7060c
BurberryC06 <christianburberry@gmail.com>
2019-01-26 01:29:14 +0000

    Added GameMode object to level & GameMode script, Added Population Controller

---
+++ TheCoders/Assets/Scripts/GameMode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+	Handles the events and scopes of different objects in the main game loop.
+	Singleton object - needs to persist between levels/waves
+*/
+public class GameMode : MonoBehaviour
+{
+	public static GameMode instance;
+	private PopulationController PopController;
+
+	// Awake is called upon construction
+	void Awake()
+	{
+	
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		Debug.Log("Start!");
+		PopController = GetComponent<PopulationController>();
+	}
+
+    // Main game loop logic here
+    void Update()
+    {
+        
+    }
+}
---
+++ TheCoders/Assets/Scripts/ResourceControllers/PopulationController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PopulationController : MonoBehaviour
+{
+	//Template for function type that is used in 
+	public delegate float Modifier(float val);
+
+	// Max population
+	[SerializeField]
+	public int PopulationMaximum { get; private set; }
+
+	// Current population
+	[SerializeField]
+	public int PopulationCurrent { get; private set; }
+
+	// Initial growth rate - per second
+	[SerializeField]
+	private float StartingGrowthRate = 1.1f;
+
+	// Current grow rate (following modifier evaluation) - per second
+	[SerializeField]
+	public float CurrentGrowRate { get; private set; }
+
+	// Have a list of modifiers that can be indexed by ID
+	public Dictionary<uint, Modifier> Modifiers;
+
+	// Add a modifier (then evaluate growth rate)
+	public void AddModifier(uint key, Modifier expr)
+	{
+		Modifiers.Add(key, expr);
+		EvaluateGrowthRate();
+	}
+
+	// Remove a modifier (then evaluate growth rate)
+	public void RemoveModifier(uint key)
+	{
+		Modifiers.Remove(key);
+		EvaluateGrowthRate();
+	}
+	
+	// Determine the current growth rate by totalling modifiers (not efficient)
+	private void EvaluateGrowthRate()
+	{
+		float newGrowthRate = 1.0f;
+
+		// Apply each modifier in the dictionary
+		foreach ( KeyValuePair<uint, Modifier> kvp in Modifiers )
+		{
+			newGrowthRate = kvp.Value(newGrowthRate);
+		}
+		CurrentGrowRate = newGrowthRate;
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		// Set current rate to 1.0f and apply initial modifier
+		CurrentGrowRate = 1.0f;
+		Modifiers = new Dictionary<uint, Modifier>();
+
+		//Add the base rate multiplier
+		Modifier BaseRateCalc = delegate (float x)
+		{
+			return (x * StartingGrowthRate);
+		};
+
+		AddModifier( 0, BaseRateCalc );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//Debug.Log(CurrentGrowRate);
+    }
+}
