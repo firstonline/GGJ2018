15bcf3dd7371af93282ab25cb38ebcfaa3cdcf56
BurberryC06 <christianburberry@gmail.com>
2019-01-26 17:03:44 +0000

    Added click functionality to planet (no animation)

---
+++ TheCoders/Assets/Scripts/Planet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+	public int PopulationGainPerClick;
+
+	//On click - do this
+	private void OnMouseDown()
+	{
+		GameMode.Instance.GetPopController().AddPopulation(PopulationGainPerClick);
+	}
+}
