d340994d483b708f1f1a9c3ea05166bee3a1642c
BurberryC06 <christianburberry@gmail.com>
2019-01-26 17:25:09 +0000

    Added simple animation for planet click and population gain

--- TheCoders/Assets/Scripts/Planet.cs
+++ TheCoders/Assets/Scripts/Planet.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-	public int PopulationGainPerClick;
+	public int PopulationGainPerClick = 1;
+	private Animator anim;
+
+	private void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
 
 	//On click - do this
 	private void OnMouseDown()
 	{
 		GameMode.Instance.GetPopController().AddPopulation(PopulationGainPerClick);
+		if (null != anim)
+		{
+			// play Bounce but start at a quarter of the way though
+			anim.Play("PlanetClicked", 0, 0.0f);
+		}
 	}
 }
